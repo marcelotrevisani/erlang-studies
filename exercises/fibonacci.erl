cb4673d425c88f0a124672563e5e99d4d4a842b8
marcelotrevisani <marceloduartetrevisani@gmail.com>
2020-09-30 16:49:01 +0100

    Add fibonacci with tail recursion - fibonacci:fb_tail/1

--- exercises/fibonacci.erl
+++ exercises/fibonacci.erl
@@ -1,5 +1,5 @@
 -module(fibonacci).
--export([fibonacci/1]).
+-export([fibonacci/1, fb_tail/1]).
 -author("Marcelo Duarte Trevisani").
 -include_lib("eunit/include/eunit.hrl").
 
@@ -7,6 +7,13 @@
 fibonacci(0) -> 0;
 fibonacci(1) -> 1;
 fibonacci(N) when N > 1 -> fibonacci(N - 1) + fibonacci(N - 2).
+
+%% Using the advantage of tail recursion
+fb_tail(N) when N >= 0 -> fb_tail(N, 0, 1).
+
+fb_tail(0, Total, _) -> Total;
+fb_tail(N, Total, Acc) -> fb_tail(N - 1, Total + Acc, Total).
+
 
 %% Unittests
 fibonacci_test_() ->
@@ -17,4 +24,14 @@ fibonacci_test_() ->
     ?_assert(fibonacci(3) =:= 2),
     ?_assert(fibonacci(10) =:= 55),
     ?_assertException(error, function_clause, fibonacci(-5))
+  ].
+  
+fibonacci_tail_test_() ->
+  [
+    ?_assert(fb_tail(0) =:= 0),
+    ?_assert(fb_tail(1) =:= 1),
+    ?_assert(fb_tail(2) =:= 1),
+    ?_assert(fb_tail(3) =:= 2),
+    ?_assert(fb_tail(10) =:= 55),
+    ?_assertException(error, function_clause, fb_tail(-5))
   ].
